a31cd89d7092bc012a7962995373c76f83de2f41
siddharthgoel94 <siddharth_goel2105@outlook.com>
2023-06-14 03:16:50 +0530

    updated app.js

--- full-house/backend/app.js
+++ full-house/backend/app.js
@@ -9,26 +9,25 @@ const Room = require('./roomSchema')
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 const PORT = 3000;
-const MONGO_URI = "mongodb://localhost:27017/full-house";
+const MONGO_URI = "mongodb+srv://siddharthgoel2105:sidgoel2105@cluster0.4bgdaxc.mongodb.net/?retryWrites=true&w=majority";
 
 app.use(cors());
 app.use(express.json());
 app.use(methodOverride('_method'))
 
-// mongoose.connect(MONGO_URI, {
-//   useNewUrlParser: true,
-// //   useCreateIndex: true,
-//   useUnifiedTopology: true,
-// });
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+//   useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
-// const db = mongoose.connection;
+const db = mongoose.connection;
 
-// db.once("open", () => {
-//   console.log("Database Connected!!!");
-// });
+db.once("open", () => {
+  console.log("Database Connected!!!");
+});
 
-// const questions = require("./questions");
-// app.use("/", questions);
+
 
 app.listen(PORT, () => {
   console.log(`listening on port : ${PORT}`);
@@ -40,14 +39,12 @@ app.post('/getRoomCount',(req,res)=>{
     console.log(req.body);
     res.redirect('/');
 })
-app.post('/updateRoomCount',async (req,res)=>{
-    // const { id } = req.params;
+app.post('/updateRoomCount/1',async (req,res)=>{
+    const { id } = req.params;
     console.log(req.body);
-    // console.log(req.body);
-    // const campground = await Campground.findByIdAndUpdate(id, { ...req.body.count });
-   
-    // await Room.save();
-    
+   await Room.findByIdAndDelete(id);
+    const room = new Room({roomId:'1',currCount:req.body.count});
+    await room.save();
     // req.flash('success', 'Successfully updated campground!');
     res.redirect('/');
     
--- full-house/backend/roomSchema.js
+++ full-house/backend/roomSchema.js
@@ -14,6 +14,10 @@ const roomSchema = Schema(
   maxCount:{
     type:Number,
     required:true
+  },
+  currentCount:{
+    type:Number
+    // required:true
   }
 }
 );
