5750ec6768012f55ffec03db57f59f8d9133437b
IRONPV03 <varshneyprerak@gmail.com>
2023-06-14 02:33:11 +0530

    Python and app.js connection successfull

--- full-house/backend/app.js
+++ full-house/backend/app.js
@@ -15,17 +15,17 @@ app.use(cors());
 app.use(express.json());
 app.use(methodOverride('_method'))
 
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-//   useCreateIndex: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect(MONGO_URI, {
+//   useNewUrlParser: true,
+// //   useCreateIndex: true,
+//   useUnifiedTopology: true,
+// });
 
-const db = mongoose.connection;
+// const db = mongoose.connection;
 
-db.once("open", () => {
-  console.log("Database Connected!!!");
-});
+// db.once("open", () => {
+//   console.log("Database Connected!!!");
+// });
 
 // const questions = require("./questions");
 // app.use("/", questions);
@@ -40,13 +40,13 @@ app.post('/getRoomCount',(req,res)=>{
     console.log(req.body);
     res.redirect('/');
 })
-app.put('/updateRoomCount/:id',async (req,res)=>{
-    const { id } = req.params;
+app.post('/updateRoomCount',async (req,res)=>{
+    // const { id } = req.params;
     console.log(req.body);
     // console.log(req.body);
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.count });
+    // const campground = await Campground.findByIdAndUpdate(id, { ...req.body.count });
    
-    await Room.save();
+    // await Room.save();
     
     // req.flash('success', 'Successfully updated campground!');
     res.redirect('/');
